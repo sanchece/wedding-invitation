2e050d59bd93d4fa74ae921aa69c83ef024ccc2a
carlos sanchez <sanchece98@gmail.com>
2022-03-08 16:11:14 -0500

    update 1

--- js/script.js
+++ js/script.js
@@ -22,11 +22,11 @@
  */
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
-    console.log('Shaadi me zaroor aana');
+    console.log('Felicidadez JOSE Y ESTER');
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("June 26, 2022 00:00:00").getTime();
+var countDownDate = new Date("June 25, 2022 14:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -53,43 +53,3 @@ var x = setInterval(function() {
     }
 }, 1000);
 
-// being a bit cool :p  
-var styles = [
-    'background: linear-gradient(#D33106, #571402)'
-    , 'border: 4px solid #3E0E02'
-    , 'color: white'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
-    , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
-    , 'line-height: 40px'
-    , 'text-align: center'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
-
-var styles1 = [
-    'color: #FF6C37'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
-
-var styles2 = [
-    'color: teal'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
-
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
-
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
-
-console.log(
-    `%cShaadi me zaroor aana!\n\n`,
-    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-)
